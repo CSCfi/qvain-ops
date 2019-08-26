2eb293c16c5be8baf80ac8194ebacd134b3380fd
Juhapekka Piiroinen <juhapekka.piiroinen@csc.fi>
2019-08-26 10:27:34 +0300

    Enable builds on macOS for Linux platform. Added helpers for stable and test deployments.

---
+++ ansible/update-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ -z ${SECRETS} ]]; then
+  SECRETS=@../../secrets-test.yaml
+fi
+
+source ../venv/bin/activate
+ansible-playbook local_build.yml -i inventories/test
+ansible-playbook site_provision.yml -i inventories/test -e ${SECRETS}
+ansible-playbook install_app.yml -i inventories/test -e ${SECRETS}
